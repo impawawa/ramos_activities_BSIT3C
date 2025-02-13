27733a1ca50cee9822d5ce8b988ee7024cf5143b
impawawa <152131860+impawawa@users.noreply.github.com>
2025-02-13 17:53:23 +0800

    Portfolio
    Design Porfolio

--- activity1/activity1/urls.py
+++ activity1/activity1/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin 
-from django.urls import path, include 
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
 
-urlpatterns = [ 
-    path('admin/', admin.site.urls), 
-    path('portfolio/', include('portfolio.urls', namespace="portfolio")), 
-] 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('portfolio/', include(('portfolio.urls', 'portfolio'), namespace="portfolio")), 
+    path('', lambda request: redirect('portfolio/', permanent=True)),  # Redirect root to portfolio
+]
+
