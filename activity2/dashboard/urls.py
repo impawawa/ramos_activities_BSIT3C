969a3ba8a11cccfb8caf485a1349cc61902b951c
impawawa <152131860+impawawa@users.noreply.github.com>
2025-02-21 13:53:48 +0800

    Simple Dashboard

---
+++ activity2/dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import index, report_view
+
+app_name = "dashboard"
+
+urlpatterns = [
+    path('', index, name='index'),   # This makes "/" work
+    path('dashboard/', index, name='dashboard'),  # Keep this for direct dashboard access
+    path('report/', report_view, name='report'),  # Ensure this matches 'report'
+]
